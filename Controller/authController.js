e4f229e4449da82464a9ec3cc30950135ef3af07
kokoleck <korinleck21@email.com>
2023-08-09 23:27:34 +0300

    connect to mongoDB

---
+++ Controller/authController.js
@@ -0,0 +1,8 @@
+
+exports.LoginPage = async (req, res) => {
+  res.render("login.ejs");
+};
+exports.RegisterPage = async (req, res) => {
+  res.render("register.ejs");
+};
+
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const path = require("path");
+const mongoose = require("mongoose");
+
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,12 +13,6 @@ app.get("/", async function (req, res) {
   res.render("index.ejs" );
 });
 
-app.get("/login", async function (req, res) {
-  res.render("login.ejs" );
-});
-app.get("/register", async function (req, res) {
-  res.render("register.ejs" );
-});
 app.get("/about", async function (req, res) {
   res.render("about.ejs" );
 });
@@ -24,7 +20,45 @@ app.get("/products", async function (req, res) {
   res.render("products.ejs" );
 });
 
+
+
+
+
+
+
+
+const authController = require("./Controller/authController");
+
+app.get("/login", authController.LoginPage);
+app.get("/register", authController.RegisterPage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 http.listen(3000, function () {
     console.log("Server is running on port 3000");
   });
   
+
+  mongoose
+  .connect("mongodb://0.0.0.0:27017/WebDevelop_1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
