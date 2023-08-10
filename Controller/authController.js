8a55c35615477461ad7508c86c9de29550ece6af
kokoleck <korinleck21@email.com>
2023-08-10 15:51:54 +0300

    create logout functionality

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,15 +1,28 @@
-
-
 const User = require("../Models/UserModel");
 
-
+exports.Logout = async (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+};
 
 exports.LoginPage = async (req, res) => {
-  res.render("login.ejs");
+  res.render("login.ejs", { loggedIn: req.session.userId });
 };
 exports.RegisterPage = async (req, res) => {
-  res.render("register.ejs");
+  res.render("register.ejs", { loggedIn: req.session.userId });
 };
+
+exports.Login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+  req.session.userId = user._id;
+  res.redirect("/");
+
+};
+
 exports.Register = async (req, res) => {
   try {
     const { email, password, fullname,phone,adress } = req.body;
@@ -19,8 +32,6 @@ exports.Register = async (req, res) => {
       return res.status(400).send("Email already registered");
     }
 
-
-    
     const user = new User({
       fullname,
       email,
@@ -30,22 +41,17 @@ exports.Register = async (req, res) => {
       isAdmin: false,
     });
     
+    // Save the user to the database
     await user.save();
 
-  
+    // Set the session
+    req.session.userId = user._id;
+    req.session.save();
+
     // Redirect to the home page
     res.redirect("/");
   } catch (err) {
     console.error(err);
     res.status(500).send("An error occurred during registration");
   }
-};
-exports.Login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-  if (!user) {
-    return res.status(401).send("Invalid email or password");
-  }
-  res.redirect("/");
-
 };
--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require("express");
+const session = require("express-session");
+
+const MongoDBStore = require("connect-mongodb-session")(session);
 const app = express();
+
+const store = new MongoDBStore({
+  uri: "mongodb://127.0.0.1/WebDevelop_1", // MongoDB connection URI
+  collection: "sessions", // Collection name to store sessions
+});
+// Catch and log any errors from the session store
+store.on("error", function (error) {
+  console.error("Session Store Error:", error);
+});
+app.use(
+  session({
+    secret: "your-secret-key",
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+  })
+);
+
 const http = require("http").Server(app);
+
+
 const path = require("path");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 const bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async function (req, res) {
-  res.render("index.ejs" );
+  res.render("index.ejs",{ loggedIn: req.session.userId} );
 });
 
 app.get("/about", async function (req, res) {
@@ -29,12 +52,11 @@ app.get("/products", async function (req, res) {
 
 
 
-
-
 const authController = require("./Controller/authController");
 
 app.get("/register", authController.RegisterPage);
 app.get("/login", authController.LoginPage);
+app.get("/logout", authController.Logout);
 
 app.post("/register", authController.Register);
 app.post("/login", authController.Login);
@@ -58,7 +80,7 @@ http.listen(3000, function () {
   
 
   mongoose
-  .connect("mongodb://0.0.0.0:27017/WebDevelop_1", {
+  .connect("mongodb://127.0.0.1/WebDevelop_1", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
